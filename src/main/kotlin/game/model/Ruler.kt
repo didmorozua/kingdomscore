76015fb2ef259aa48ff72a14a7e9876c32f4b12a
Vlad <vladdedmoroz6661998@gmail.com>
2026-08-17 16:18:30 +0200

    Add Ruler model and DynastyManager

--- src/main/kotlin/game/model/Ruler.kt
+++ src/main/kotlin/game/model/Ruler.kt
@@ -1,5 +1,5 @@
 ﻿package game.model
-enum class Trait(val displayName: String, val taxBonus: Double) {
+enum class Trait(val displayName: String, val taxBonus: Double = 0.0) {
     GREEDY("Greedy (+20% Gold)", 0.20),
     WARRIOR("Warrior (+10% Army Power)", 0.0),
     BUILDER("Builder (-10% Construct Cost)", 0.0),
@@ -7,6 +7,23 @@ enum class Trait(val displayName: String, val taxBonus: Double) {
 }
 data class Ruler(
     val name: String,
-    var age: Int = 25,
-    val trait: Trait
+    val title: String,
+    val militaryBonus: Int,
+    val economicBonus: Long,
+    val trait: Trait = Trait.WARRIOR
 )
+class DynastyManager {
+    var currentRuler: Ruler = Ruler(
+        name = "Arthur I",
+        title = "The Just",
+        militaryBonus = 10,
+        economicBonus = 15,
+        trait = Trait.WARRIOR
+    )
+    fun printRulerInfo() {
+        println("👑 Current Ruler: King ${currentRuler.name} ${currentRuler.title}")
+        println("   * Trait: ${currentRuler.trait.displayName}")
+        println("   * Military Buff: +${currentRuler.militaryBonus} Power")
+        println("   * Economic Buff: +${currentRuler.economicBonus} Gold/turn")
+    }
+}
